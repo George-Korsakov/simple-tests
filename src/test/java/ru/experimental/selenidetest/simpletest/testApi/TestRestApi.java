f80caf9081939e4a02258d8ec18b304da838c1bf
voland <voland2k@gmail.com>
2021-05-10 23:17:26 +0300

    добавлен пример теста простой запрос с Unirest и проверкой кода ответа

--- src/test/java/ru/experimental/selenidetest/simpletest/testApi/TestRestApi.java
+++ src/test/java/ru/experimental/selenidetest/simpletest/testApi/TestRestApi.java
@@ -1,9 +1,14 @@
 package ru.experimental.selenidetest.simpletest.testApi;
 import io.restassured.RestAssured;
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import ru.experimental.selenidetest.simpletest.TestBase;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.CoreMatchers.hasItem;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class TestRestApi extends TestBase {
@@ -29,11 +34,23 @@ public class TestRestApi extends TestBase {
                 .header("x-rapidapi-key", x_rapidapi_key);
 
         when()
-                .get(EndpointURL.RANDOM.addPath(String.format("/%s?json=true&fragment=true&max=%s&min=5s",type, max, min)))
+                .get(EndpointURL.RANDOM.addPath(String.format("/%s?json=true&fragment=true&max=%s&min=%s",type, max, min)))
                 .then()
                     .statusCode(200)
                     .body("type", hasItem("trivia"));
 
+    }
+
+    @Test
+    public void test2() {
+        // простой запрос с Unirest и проверкой кода ответа
+        HttpResponse<String> response = Unirest.get("https://numbersapi.p.rapidapi.com/random/trivia?json=true&fragment=false&max=20&min=10")
+                .header("x-rapidapi-host", "numbersapi.p.rapidapi.com")
+                .header("x-rapidapi-key", "6cad337ed5msh7b87fd9d62c0ddbp1e0d2fjsn9aebde718718")
+                .header("cache-control", "no-cache")
+                .asString();
+        System.out.println("HttpResponse: " + response.getStatusText() + " " + response.getBody());
+        assertEquals("OK", response.getStatusText() );
     }
 
 }
