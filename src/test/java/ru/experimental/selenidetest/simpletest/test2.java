e01bac506b7f361e93d07530866e9bd3269aae7a
voland <voland2k@gmail.com>
2022-01-17 20:27:49 +0300

    прсотой тест на страницу списка задач

--- src/test/java/ru/experimental/selenidetest/simpletest/test2.java
+++ src/test/java/ru/experimental/selenidetest/simpletest/test2.java
@@ -1,6 +1,7 @@
 package ru.experimental.selenidetest.simpletest;
 
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -31,6 +32,24 @@ public class test2 {
         $((".footer")).find(".filters").find("All").click();
         sleep(3000);
         $$(".todo-list").shouldHaveSize(3);
+
+    }
+    @Test
+    public void test3 () {
+        // создаем 4 задачи
+        $(".new-todo").setValue("Test task 1").pressEnter();
+        $(".new-todo").setValue("Test task 2").pressEnter();
+        $(".new-todo").setValue("Test task 3").pressEnter();
+        $(".new-todo").setValue("Test task 4").pressEnter();
+        // выбираем все задачи
+        $x("//a[normalize-space()='All']").click();
+        sleep(3000);
+        // отметить первую задачу
+        $x("//li[1]//div[1]//input[1]").click();
+        // проверка, что стаутс задачи выполнено
+        Assertions.assertTrue($x("//li[@class='completed']").exists());
+        // проверяем, что в сиске оставлось 2 активных задачи
+        $$(".strong[data-reactid='.0.2.0.0']").contains(3);
 
     }
 
